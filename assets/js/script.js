80726c9c6b5686bce82da0c0f1bede1e3dafb085
VesNa <nat.veselowa2010@yandex.ru>
2024-01-21 15:18:22 -0500

    added color-code to each timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,44 @@ $('#currentDay').text(dayjs().format('dddd, MMMM Do'));
 let workHours = [];
 let startHour = 9;
 let workingDay = 9;
+let currentTime = parseInt(dayjs().format('H'));
+
 for (let i=0; i<workingDay; i++) {
-    let hour = dayjs().hour(startHour).format('hA');
+    let hour = {time: dayjs().hour(startHour).format('hA'),
+                value: dayjs().hour(startHour).format('H'),}
     workHours.push(hour);
     startHour++;
 };
 
-for (let i=0; i<workHours.length; i++) {
+$('.container').addClass('mt-5');
+
+for (let index=0; index<workHours.length; index++) {    
     $('.container').append(
         `<div class='row'>
-        <div class='hour col-1'>${workHours[i]}</div>
+        <div class='hour col-1'>${workHours[index].time}</div>
         <textarea class='textarea col-10'></textarea>
-        <button class='saveBtn col-1 d-flex'></button>
+        <button class='saveBtn col-1'></button>
         </div>`
-    )
+    );
+    let hour =parseInt(workHours[index].value);
+    let textarea = $('.container').find('.row').eq(index).find('.textarea');
+    if (hour == currentTime) {
+        textarea.addClass('present');               
+    } else if (hour > currentTime) {
+        textarea.addClass('future');        
+    } else {
+        textarea.addClass('past');
+    };    
 };
+
+
+console.log(currentTime);
+console.log(workHours);
+
+
+
+
+
 
 
 
